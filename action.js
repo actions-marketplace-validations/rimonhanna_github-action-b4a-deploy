1268998c3f3588328a5d45beb485e2e108b57aae
Rimon Hanna <rimon.hanna@live.com>
2021-12-10 16:25:36 +0100

    Update action.js

--- action.js
+++ action.js
@@ -14,12 +14,13 @@ Toolkit.run(async tools => {
 
 
   try {
-    execSync('curl https://raw.githubusercontent.com/back4app/parse-cli/back4app/installer.sh | sudo /bin/bash').toString().trim()
-    execSync('mkdir -p ~/.back4app').toString().trim()
-    execSync(`echo -e "machine parsecli.back4app.com\n  login default\n  password ${b4aApiKey}" > ~/.back4app/netrc`).toString().trim()
+    execSync('curl https://raw.githubusercontent.com/back4app/parse-cli/back4app/installer.sh | sudo /bin/bash')
+    execSync('mkdir -p ~/.back4app')
+    execSync(`echo -e "machine parsecli.back4app.com\n  login default\n  password ${b4aApiKey}" > ~/.back4app/netrc`)
+    execSync('cat ~/.back4app/netrc')
 
     if(appName && appName.length > 0) {
-      execSync(`b4a default "${appName}"`).toString().trim()
+      execSync(`b4a default "${appName}"`)
     }
 
     let deploy = execSync('b4a deploy').toString().trim()
